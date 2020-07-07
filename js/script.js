19b2f075c71e51bc619198656d650e81f1990ab5
Valerio <valerio.modesti@gmail.com>
2020-07-07 16:39:36 +0200

    biglietto-treno

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var kmTragitto = parseInt(prompt('Quanti km vuole percorrere?'));
+var eta = parseInt(prompt('Quanti anni ha?'));
+var bigliettoIntero = kmTragitto * 0.21;
+var sconto;
+
+if (eta < 18){
+    sconto = bigliettoIntero * 20 / 100;
+} else if (eta > 65){
+    sconto = bigliettoIntero * 40 / 100;
+} else {
+    sconto = 0;
+}
+
+var prezzoFinale = parseInt(bigliettoIntero - sconto);
+
+document.getElementById('effettivo').innerHTML = prezzoFinale + ' ' + 'euro';
